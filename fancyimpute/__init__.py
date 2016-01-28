1c76668ee02203caedf12a3cd6173a20c50be6c1
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-01-28 14:05:26 -0500

    renaming MICE

--- fancyimpute/__init__.py
+++ fancyimpute/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-
 from .solver import Solver
 from .nuclear_norm_minimization import NuclearNormMinimization
 from .bayesian_ridge_regression import BayesianRidgeRegression
