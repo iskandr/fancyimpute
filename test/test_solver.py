de6ab1140918904e07f44f4d86a852a7feb6e8a6
drkarthi <karthikthegoth@gmail.com>
2018-08-29 18:49:04 -0500

    add tests for complete matrix warning

---
+++ test/test_solver.py
@@ -0,0 +1,21 @@
+from fancyimpute.solver import Solver
+import numpy as np
+import warnings
+
+
+def test_prepare_input_data():
+	_solver = Solver()
+	# test that a complete matrix returns a warning
+	X1 = np.zeros((5, 5))
+	with warnings.catch_warnings(record=True) as w:
+		_solver.prepare_input_data(X1)
+		assert str(w[0].message) == "Input matrix is not missing any values", "Warning is not generated for a complete matrix"
+	# test that an incomplete matrix does not return a warning
+	X2 = np.zeros((5, 5))
+	X2[2, 3] = None
+	with warnings.catch_warnings(record=True) as w:
+		_solver.prepare_input_data(X2)
+		assert len(w) == 0, "Warning is generated for an incomplete matrix"
+
+if __name__ == "__main__":
+    test_prepare_input_data()
