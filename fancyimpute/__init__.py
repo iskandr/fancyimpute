65f2db5942a085b98815bb7f186bb6f03643c6d0
Sergey Feldman <sergeyfeldman@gmail.com>
2019-07-13 10:29:42 -0500

    Adding iterative imputer stub and bumping version (#113)
    * Adding iterative imputer stub and bumping version
    
    * Update __init__.py

--- fancyimpute/__init__.py
+++ fancyimpute/__init__.py
@@ -10,7 +10,11 @@ from .scaler import BiScaler
 from .knn import KNN
 from .similarity_weighted_averaging import SimilarityWeightedAveraging
 
-__version__ = "0.5.2"
+# while iterative imputer is experimental in sklearn, we need this
+from sklearn.experimental import enable_iterative_imputer 
+from sklearn.impute import IterativeImputer
+
+__version__ = "0.5.3"
 
 __all__ = [
     "Solver",
@@ -21,5 +25,6 @@ __all__ = [
     "SoftImpute",
     "BiScaler",
     "KNN",
-    "SimilarityWeightedAveraging"
+    "SimilarityWeightedAveraging",
+    "IterativeImputer"
 ]
